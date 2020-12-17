ac6398bc68ca0a148cf058b391e097210f760a64
TamaraSavoiniLavarda <tamarasavoini997@gmail.com>
2020-12-17 19:15:21 -0300

    cambios marcelo

--- parts/pag-imagenes.php
+++ parts/pag-imagenes.php
@@ -9,37 +9,52 @@
         </div>
     </section>
 
+	
     <!-- Imagenes -->
     <section class="cabure-reservas">
         <div class="cabure-contenedor__mod">
             <div class="container-fluid mt-5">
                 <div class="row">
-                    <div class="col-lg-6">
+					<?php $galerias = obtenerElementoWidget('Siatrasag_Galeria');
+						//600x300
+						$color = true;
+					    $count = 0;
+						$estilo = '';
+					?>
+					<?php foreach($galerias as $galeria):
+						if($color){
+							$estilo = 'cabure-galeria-titulo-azul';
+						}else{
+							$estilo = 'cabure-galeria-titulo-verde';
+						}
+						$color = !$color;
+						
+						$count = ($count >= 1)? 0 : $count;
+					?>
+					<div class="col-lg-6">
                         <div class="cabure_galeria">
-                            <div class="cabure-galeria-titulo-azul">
-                                Mantenimiento predio Lago Salto Grande
-                            </div>
-                            <?php
-                                echo do_shortcode('[smartslider3 slider="9"]');
-                            ?>
+                            <div class="<?=$estilo;?>"><?= $galeria->title;?></div>
+							<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+								<div class="carousel-inner">
+									<?php foreach($galeria->ids as $img => $id): 
+										$claseCss = ($count == 0)? 'class="carousel-item active"' : 'class="carousel-item"';
+									 	//$imagen = wp_get_attachment_image($id, $galeria->size);
+									 	//768x576
+									 	$imagen = wp_get_attachment_image($id,['700','500']);
+									?>
+										<div <?=$claseCss?>> <?=$imagen;?> </div>
+									<?php $count++; endforeach; ?>
+								</div>
+							</div>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="cabure_galeria">
-                            <div class="cabure-galeria-titulo-verde">
-                                Acciones de mejora en nuestro predio del Lago
-                            </div>
-                            <?php
-                                echo do_shortcode('[smartslider3 slider="10"]');
-                            ?>
-                        </div>
-                    </div>
+					<?php endforeach; ?>
                 </div>
-                <div class="col-lg-12 cabure-imagenes-verMas">
+<!--                 <div class="col-lg-12 cabure-imagenes-verMas">
                     <button class="cabure-home-verMas-btn">
                     + + + + + <span>&nbsp;&nbsp;Ver más galerías&nbsp;&nbsp;</span> + + + + +
                     </button>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
